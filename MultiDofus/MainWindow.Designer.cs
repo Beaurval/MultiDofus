b9e40b6233b658b13fb1ddfffe35dfbeca1230e0
Valentin <Beaury.valentin@gmail.com>
2021-05-20 01:01:28 +0200

    Sizing and style

--- MultiDofus/MainWindow.Designer.cs
+++ MultiDofus/MainWindow.Designer.cs
@@ -30,30 +30,64 @@ namespace MultiDofus
         private void InitializeComponent()
         {
             this.ListeBtn = new System.Windows.Forms.TableLayoutPanel();
+            this.topBar = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ListeBtn
             // 
+            this.ListeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListeBtn.BackColor = System.Drawing.Color.White;
             this.ListeBtn.ColumnCount = 1;
-            this.ListeBtn.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.ListeBtn.Location = new System.Drawing.Point(98, 12);
+            this.ListeBtn.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.ListeBtn.Location = new System.Drawing.Point(3, 29);
             this.ListeBtn.Name = "ListeBtn";
             this.ListeBtn.RowCount = 1;
-            this.ListeBtn.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.ListeBtn.Size = new System.Drawing.Size(80, 356);
+            this.ListeBtn.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.ListeBtn.Size = new System.Drawing.Size(54, 321);
             this.ListeBtn.TabIndex = 0;
+            // 
+            // topBar
+            // 
+            this.topBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.topBar.Location = new System.Drawing.Point(3, 3);
+            this.topBar.Name = "topBar";
+            this.topBar.Size = new System.Drawing.Size(54, 20);
+            this.topBar.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.topBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ListeBtn, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.365439F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.63456F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(60, 353);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 380);
-            this.Controls.Add(this.ListeBtn);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(60, 353);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1, 1);
             this.Name = "MainWindow";
             this.Text = "Multi Dofus";
             this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -61,6 +95,8 @@ namespace MultiDofus
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel ListeBtn;
+        private System.Windows.Forms.Panel topBar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- MultiDofus/MainWindow.cs
+++ MultiDofus/MainWindow.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,38 +14,82 @@ namespace MultiDofus
 {
     public partial class MainWindow : Form
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, UIntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseCapture(IntPtr hwnd);
+
+        const uint WM_SYSCOMMAND = 0x112;
+        const uint MOUSE_MOVE = 0xF012;
+
         private readonly List<Perso> _personnages;
         public MainWindow(List<Perso> personnages)
         {
-            this.BackColor = Color.FromArgb(25,26,17);
-            
-            InitializeComponent();
             _personnages = personnages;
+            InitializeComponent();
+
+            //Sizing
+            this.ListeBtn.Height = 60 * (_personnages.Count);
+            this.ListeBtn.Width = this.Width = this.topBar.Width = 60;
+            this.Height = ListeBtn.Height + topBar.Height + 10;
+            this.Width = ListeBtn.Width + 20;
+            
+            //margin
+            this.topBar.Margin = new Padding(0, 0, 0, 5);
+            this.ListeBtn.Margin = new Padding(0, 0, 0, 5);
+
+            //Color
+            this.BackColor = Color.FromArgb(25, 26, 17);
+            this.ListeBtn.BackColor = Color.FromArgb(25, 26, 17);
+            this.topBar.BackColor = Color.FromArgb(58, 59, 59);
+            this.topBar.MouseDown += new MouseEventHandler(holdTopBar);
 
             ListeBtn.RowCount = _personnages.Count;
             ListeBtn.ColumnCount = 1;
-            ListeBtn.Anchor = (((((AnchorStyles.Top | AnchorStyles.Bottom)
-                    | AnchorStyles.Left)
-                    | AnchorStyles.Right)));
+
             
+
+            ListeBtn.ColumnStyles[0].SizeType = SizeType.Absolute;
+            ListeBtn.ColumnStyles[0].Width = 60;
 
             for (int i = 0; i < _personnages.Count; i++)
             {
                 var perso = _personnages[i];
-                ListeBtn.RowStyles.Add(new RowStyle(SizeType.Absolute,ListeBtn.Height / _personnages.Count ));
+                ListeBtn.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));     
 
                 Button btn = new Button();
-                btn.BackColor = Color.White;
+                btn.BackColor = Color.FromArgb(80, 80, 80);
+
                 btn.Click += (sender, EventArgs) => { openWindow_Click(sender, EventArgs, perso); };
-                btn.Anchor = (AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom | AnchorStyles.Left);
+
                 btn.AutoSize = true;
-                btn.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.Feca));
+                btn.BackgroundImage = ((Image)(Properties.Resources.Feca));
                 btn.BackgroundImageLayout = ImageLayout.Center;
-                            
+
+                btn.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom);
+                btn.Margin = new Padding(0, 0, 0, 0);
                 ListeBtn.Controls.Add(btn, 0, 1);
             }
+
         }
 
+        // ----------- MOUSE CONTROL TOP BAR
+        public void DragMe()
+        {
+            DefWindowProc(this.Handle, WM_SYSCOMMAND, (UIntPtr)MOUSE_MOVE, IntPtr.Zero);
+        }
+        private void holdTopBar(object sender, MouseEventArgs e)
+        {
+            Control ctl = sender as Control;
+
+            // when we get a buttondown message from a button control
+            // the button has capture, we need to release capture so
+            // or DragMe() won't work.
+            ReleaseCapture(ctl.Handle);
+
+            this.DragMe(); // put the form into mousedrag mode.
+        }
+        // ----------- MOUSE CONTROL TOP BAR ////
 
         private void openWindow_Click(object sender, EventArgs e, Perso perso)
         {
